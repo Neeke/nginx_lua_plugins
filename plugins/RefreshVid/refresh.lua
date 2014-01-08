69c84a889860f4c4a6e65edb2e9fcb4f93ac3f27
Neeke <ciogao@gmail.com>
2014-01-08 14:16:34 +0800

    update refresh.lua

--- plugins/RefreshVid/refresh.lua
+++ plugins/RefreshVid/refresh.lua
@@ -6,12 +6,6 @@ local timeout = 20
 local ext = 'html'
 local httpcode = 301
 
-time = ngx.now() - ngx.var.arg_t
-
-if static == 2 then
-        local ext = 'php'
-end
-
 -- uri like /v1_1/mBSg4/1107/112.html
 
 local m,err = ngx.re.match(ngx.var.uri,"(v1_1)/([0-9a-zA-Z]+)/(?<tid>[0-9a-zA-Z]+)/(?<gid>[0-9a-zA-Z]+)")
@@ -19,16 +13,24 @@ local m,err = ngx.re.match(ngx.var.uri,"(v1_1)/([0-9a-zA-Z]+)/(?<tid>[0-9a-zA-Z]
 if err == nil then
         local tid = m['tid']
         local gid = m['gid']
+        time = ngx.now() - ngx.var.arg_t
+
+        if static == 2 then
+                ext = 'php'
+        end
 
         if time < timeout then
                 -- access
                 local uri = "/" .. ext .. "/" .. tid .. "/" .. gid .. "." .. ext
-                ngx.req.set_uri(uri)
+
+                local res = ngx.location.capture(uri,{args = ngx.req.get_uri_args()})
+                ngx.print(res.body)
+                ngx.exit(res.status)
         else
+                -- timeout & redirect
                 local args = ngx.req.get_uri_args()
 
                 local uri = "/v1" .. "/" .. tid .. "/" .. gid .. ".html?"
-                --local i = 0
 
                 for key, val in pairs(args) do
                         if (key == "vid" or key == "sign" or key == "t" or key == "" or val == "" or type(val) == "table") then
